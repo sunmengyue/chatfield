43529bbfdbb8316fb2c8ad05ea9fb9b7cd9a864e
sunmengyue <mengyuesun@gmail.com>
2021-08-19 20:13:03 -0400

    emit chat-message event

--- index.js
+++ index.js
@@ -13,9 +13,8 @@ app.get('/', (req, res) => {
 // intergrating socket.io
 const io = new Server(server);
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on('chat message', (msg) => {
+    console.log(`message: ${msg}`);
   });
 });
 
